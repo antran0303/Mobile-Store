9c77bb0820c0abef872684ca302ea52091716d1f
An Tran <antoan03031999@gmail.com>
2023-03-06 16:48:22 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          An Tran hoc react
         </a>
       </header>
     </div>
